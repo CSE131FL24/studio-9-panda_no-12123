059901b6cb0d66f8e13ac01cc4601191bd2c0aeb
Zoejane636 <y.yiding@wustl.edu>
2024-11-20 13:53:52 -0600

    1

--- src/studio9/Polynomial.java
+++ src/studio9/Polynomial.java
@@ -3,98 +3,110 @@ package studio9;
 import java.util.LinkedList;
 
 public class Polynomial {
-	
-	private LinkedList<Double> list;
 
-	/**
-	 * Constructs a Polynomial with no terms yet.
-	 */
-	public Polynomial() {
-		//FIXME
-	}
+    private LinkedList<Double> list;
 
-	
-	/**
-	 * 
-	 * @param coeff
-	 * @return polynomial with added term
-	 */
-	public void addTerm(double coeff) {
-		//FIXME
-	}
-	
-	/*
-	 * Returns a String of the polynomial with the proper form:
-	 * 
-	 * Cx^N + Cx^N-1 + ... + Cx + C
-	 */
-	public String toString() {
-		return ""; //FIXME
-	}
-	
-	/**
-	 * 
-	 * @param x
-	 * @return value of polynomial at that x
-	 */
-	public double evaluate(double x) {
-		return 0;//FIXME
-	}
+    /**
+     * Constructs a Polynomial with no terms yet.
+     */
+    public Polynomial() {
+        list = new LinkedList<>(); // Initialize an empty list
+    }
 
-	
-	public Polynomial derivative() {
-		return null;//FIXME
-	}
-	
+    /**
+     * Adds a term to the polynomial.
+     * 
+     * @param coeff the coefficient of the term to add
+     */
+    public void addTerm(double coeff) {
+        list.add(coeff);
+    }
 
-	/**
-	 * This is the "equals" method that is called by
-	 *    assertEquals(...) from your JUnit test code.
-	 *    It must be prepared to compare this Polynomial
-	 *    with any other kind of Object (obj).  Eclipse
-	 *    automatically generated the code for this method,
-	 *    after I told it to use the contained list as the basis
-	 *    of equality testing.  I have annotated the code to show
-	 *    what is going on.
-	 */
+    /**
+     * Returns a String representation of the polynomial in the proper form:
+     * Cx^N + Cx^N-1 + ... + Cx + C
+     */
+    @Override
+    public String toString() {
+        if (list.isEmpty()) {
+            return "0";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        int degree = list.size() - 1;
+        
+        for (int i = 0; i < list.size(); i++) {
+            double coeff = list.get(i);
+            
+            if (coeff != 0) {
+                if (sb.length() > 0 && coeff > 0) {
+                    sb.append(" + ");
+                } else if (coeff < 0) {
+                    sb.append(" - ");
+                }
 
-	public boolean equals(Object obj) {
-		// If the two objects are exactly the same object,
-		//    we are required to return true.  The == operator
-		//    tests whether they are exactly the same object.
-		if (this == obj)
-			return true;
-		// "this" object cannot be null (or we would have
-		//    experienced a null-pointer exception by now), but
-		//    obj can be null.  We are required to say the two
-		//    objects are not the same if obj is null.
-		if (obj == null)
-			return false;
+                double absCoeff = Math.abs(coeff);
+                if (absCoeff != 1 || degree - i == 0) {
+                    sb.append(absCoeff);
+                }
 
-		//  The two objects must be Polynomials (or better) to
-		//     allow meaningful comparison.
-		if (!(obj instanceof Polynomial))
-			return false;
+                if (degree - i > 0) {
+                    sb.append("x");
+                    if (degree - i > 1) {
+                        sb.append("^").append(degree - i);
+                    }
+                }
+            }
+        }
+        
+        return sb.toString();
+    }
 
-		// View the obj reference now as the Polynomial we know
-		//   it to be.  This works even if obj is a BetterPolynomial.
-		Polynomial other = (Polynomial) obj;
+    /**
+     * Evaluates the polynomial for a given value of x.
+     * 
+     * @param x the value at which to evaluate the polynomial
+     * @return the evaluated value
+     */
+    public double evaluate(double x) {
+        double result = 0;
+        int degree = list.size() - 1;
 
-		//
-		// If we get here, we have two Polynomial objects,
-		//   this and other,
-		//   and neither is null.  Eclipse generated code
-		//   to make sure that the Polynomial's list references
-		//   are non-null, but we can prove they are not null
-		//   because the constructor sets them to some object.
-		//   I cleaned up that code to make this read better.
+        for (int i = 0; i < list.size(); i++) {
+            result += list.get(i) * Math.pow(x, degree - i);
+        }
+        
+        return result;
+    }
 
+    /**
+     * Computes the derivative of the polynomial.
+     * 
+     * @return a new Polynomial representing the derivative
+     */
+    public Polynomial derivative() {
+        Polynomial derivative = new Polynomial();
+        int degree = list.size() - 1;
 
-		// A LinkedList object is programmed to compare itself
-		//   against any other LinkedList object by checking
-		//   that the elements in each list agree.
+        for (int i = 0; i < list.size() - 1; i++) {
+            double coeff = list.get(i);
+            derivative.addTerm(coeff * (degree - i));
+        }
 
-		return this.list.equals(other.list);
-	}
+        return derivative;
+    }
 
+    /**
+     * Checks equality between this polynomial and another object.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || !(obj instanceof Polynomial))
+            return false;
+        
+        Polynomial other = (Polynomial) obj;
+        return this.list.equals(other.list);
+    }
 }
--- src/studio9/WordCount.java
+++ src/studio9/WordCount.java
@@ -5,48 +5,54 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import support.cse131.NotYetImplementedException;
-
 public class WordCount {
-	/**
-	 * Constructs and returns a map of the distinct words in the specified list with
-	 * each word associated with its accumulated count (that is: the number of
-	 * occurrences of the word in the list).
-	 * 
-	 * For example, if passed a List<String> containing:
-	 * 
-	 * [to, be, or, not, to, be]
-	 * 
-	 * the resulting Map<String, Integer> would contain
-	 * 
-	 * key="to", value=2;
-	 * key="be", value=2;
-	 * key="or", value=1;
-	 * key="not", value=1;
-	 * 
-	 * @param words
-	 * @return a map which contains all of the distinct words as keys, each
-	 *         associated with the number of occurrences of the word
-	 */
-	public static Map<String, Integer> countWords(List<String> words) {
 
-		// FIXME
-		throw new NotYetImplementedException();
+    /**
+     * Constructs and returns a map of the distinct words in the specified list with
+     * each word associated with its accumulated count (that is: the number of
+     * occurrences of the word in the list).
+     * 
+     * For example, if passed a List<String> containing:
+     * 
+     * [to, be, or, not, to, be]
+     * 
+     * the resulting Map<String, Integer> would contain
+     * 
+     * key="to", value=2;
+     * key="be", value=2;
+     * key="or", value=1;
+     * key="not", value=1;
+     * 
+     * @param words
+     * @return a map which contains all of the distinct words as keys, each
+     *         associated with the number of occurrences of the word
+     */
+    public static Map<String, Integer> countWords(List<String> words) {
+        Map<String, Integer> wordCountMap = new HashMap<>();
 
-	}
-	
-	public static void main(String[] args) {
-		
-		List<String> wordList = new LinkedList<>();
-		wordList.add("to");
-		wordList.add("be");
-		wordList.add("or");
-		wordList.add("not");
-		wordList.add("to");
-		wordList.add("be");
-		Map<String, Integer> words = countWords(wordList);
-		
-		//TODO: Write code that will iterate over the words map
-		//to verify its contents
-	}
+        for (String word : words) {
+            wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+        }
+
+        return wordCountMap;
+    }
+
+    public static void main(String[] args) {
+        // Sample word list
+        List<String> wordList = new LinkedList<>();
+        wordList.add("to");
+        wordList.add("be");
+        wordList.add("or");
+        wordList.add("not");
+        wordList.add("to");
+        wordList.add("be");
+
+        // Count words
+        Map<String, Integer> words = countWords(wordList);
+
+        // Iterate over the map to verify its contents
+        for (Map.Entry<String, Integer> entry : words.entrySet()) {
+            System.out.println("Word: " + entry.getKey() + ", Count: " + entry.getValue());
+        }
+    }
 }
